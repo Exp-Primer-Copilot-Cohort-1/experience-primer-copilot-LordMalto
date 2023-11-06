2b9adbb75b25ccdaf05160a056e5c41454d54d65
Malte <101904063+LordMalto@users.noreply.github.com>
2023-11-06 21:49:00 +0000

    Copilot thi rd commit

--- comments.js
+++ comments.js
@@ -1,60 +1,31 @@
 // Create a web server
-// Run it by node comments.js
-// Access it via http://localhost:3000
-
-// Load the http module to create an http server.
-var http = require('http');
+var express = require('express');
+var router = express.Router();
 var fs = require('fs');
-var qs = require('querystring');
-var comments = require('./comments.json');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
+var app = express();
+var comments = require('../comments.json');
+var path = require('path');
 
-// Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
-  var url = request.url;
-  var method = request.method;
-  var body = '';
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-  if (url === '/') {
-    if (method === 'GET') {
-      response.writeHead(200, {'Content-Type': 'text/html'});
-      fs.readFile('index.html', 'utf8', function(err, data) {
-        if (err) throw err;
-        response.write(data);
-        response.end();
-      });
-    }
-  }
-  else if (url === '/comments') {
-    if (method === 'GET') {
-      response.writeHead(200, {'Content-Type': 'application/json'});
-      response.write(JSON.stringify(comments));
-      response.end();
-    }
-    else if (method === 'POST') {
-      request.on('data', function (chunk) {
-        body += chunk;
-      });
-      request.on('end', function () {
-        var obj = qs.parse(body);
-        comments.push(obj);
-        fs.writeFile('comments.json', JSON.stringify(comments), function(err) {
-          if (err) throw err;
-          console.log('It\'s saved!');
-        });
-        response.writeHead(200, {'Content-Type': 'application/json'});
-        response.write(JSON.stringify(comments));
-        response.end();
-      });
-    }
-  }
-  else {
-    response.writeHead(404, {'Content-Type': 'text/plain'});
-    response.write('404 Not Found\n');
-    response.end();
-  }
+// GET /comments
+router.get('/', function(req, res) {
+  res.json(comments);
 });
 
-// Listen on port 3000, IP defaults to
+// POST /comments
+router.post('/', function(req, res) {
+  comments.push(req.body);
+  fs.writeFile(path.join(__dirname, '../comments.json'), JSON.stringify(comments), function(err) {
+    if (err) {
+      console.log('Error: ', err);
+    }
+  });
+  res.json(comments);
+});
 
-
-
+module.exports = router;
